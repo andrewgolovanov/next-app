a7816f11bc2f623ac63c75e351b76106f8a3d0c6
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-08-23 17:28:30 +0400

    chore: add small adjustments

---
+++ app/page/[id]/page.tsx
@@ -0,0 +1,13 @@
+export default function Page({ searchParams }) {
+  return (
+    <div>
+      <h1>Search Params: {JSON.stringify(searchParams)}</h1>
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  return {
+    slug: 'test',
+  };
+}
