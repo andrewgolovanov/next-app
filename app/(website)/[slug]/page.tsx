8e581849aff50b96a0c72db00b6f46df561a891e
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-08-23 17:47:34 +0400

    test: root layout

---
+++ app/(website)/[slug]/page.tsx
@@ -0,0 +1,17 @@
+export default function Page({ searchParams }) {
+  console.log(searchParams);
+
+  return (
+    <div>
+      <h1>Search Params: {JSON.stringify(searchParams)}</h1>
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  return [
+    {
+      slug: 'test',
+    },
+  ];
+}
