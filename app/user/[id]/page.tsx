eed1d5852e16306ff01e01c20cd075988ce16bb7
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-08-24 16:02:01 +0400

    chore: add small adjustments

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,11 +1,12 @@
 /*
   NOTE:
-  This page shows the case when search params is empty if you do not interact with it in any way on the page
+  This page shows the case when search parameters become available
 */
 
 export default function Page({ searchParams }) {
   console.log(searchParams);
 
+  // It is enough just to start interacting with the searchParams object
   return <div>Search Params: {JSON.stringify(searchParams)}</div>;
 }
 
