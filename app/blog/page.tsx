e8be338f8a2cc45b56d0f9131af5ff5ae6d54eb6
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-08-24 17:37:18 +0400

    chore: add the latest version of canary

--- app/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function BlogPage({ searchParams }) {
-  console.log(searchParams);
-
-  return (
-    <div>
-      <h1>Search Params: {JSON.stringify(searchParams)}</h1>
-    </div>
-  );
-}
